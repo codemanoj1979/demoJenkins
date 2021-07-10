7e0f81e150adc747e4bf6f06fd6fb5b30032ba07
Naresh Sharma <Naresh_Sharma@epam.com>
2021-07-10 18:18:19 +0530

    webdriver launch

--- src/test/java/test/TestLogin.java
+++ src/test/java/test/TestLogin.java
@@ -1,10 +1,21 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestLogin {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("test login class");
+		System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.quit();
 		
 	}
 
